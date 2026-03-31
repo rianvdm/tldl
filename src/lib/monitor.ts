077c7f944602258fc901ca7fe4c897e9e142ebf1
Rian van der Merwe <rianvdm@gmail.com>
2026-03-31 16:46:37 -0700

    Revert "fix: remove title-based dedup from add flow queue-latest"
    This reverts commit bb15e96836db5e49187edb35d309338508765d91.

--- src/lib/monitor.ts
+++ src/lib/monitor.ts
@@ -178,6 +178,7 @@ export async function addPodcastToMonitoring(
         const episodeId = `${podcastId}_${latestEpisode.id}`;
 
         const existingEpisode = await getEpisode(env.TLDL_DATA, episodeId);
+        const existsByTitle = await episodeExistsByTitle(env, podcastId, latestEpisode.title);
 
         console.log(JSON.stringify({
             event: "monitor_add_queue_latest_check",
@@ -186,9 +187,10 @@ export async function addPodcastToMonitoring(
             episodeTitle: latestEpisode.title,
             episodeGuid: latestEpisode.guid,
             existsById: !!existingEpisode,
+            existsByTitle: existsByTitle,
         }));
 
-        if (!existingEpisode) {
+        if (!existingEpisode && !existsByTitle) {
             await queueEpisodeForProcessing(env, {
                 podcastId,
                 episodeId,
